9a81946dec8ba4d83102237c290a7dbb681aa65e
kenzop <43009804+kenzop@users.noreply.github.com>
2019-06-06 09:41:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ RECT_CENTER_Y = 200
 RECT_DISTANCE_FROM_CENTER = 250
 PLAYER_MOVEMENT_BORDERS = 20
 SPEAR_COUNT1 = 7
+SPEAR_COUNT2 = 5
+SPIKE_COUNT1 = 2
+TIMER = 30
+TIMER_INCREMENT = 0
 
 
 class Menu:
@@ -55,7 +59,7 @@ class Instructions:
         arcade.draw_text("INSTRUCTIONS", 150, 525, arcade.color.WHITE, 50)
         arcade.draw_text("In this game, you play as a heart.  Use the arrow keys to move", 50, 470, arcade.color.WHITE, FONT_SIZE)
         arcade.draw_text("Avoid attacks at all costs! Keep dodging until time runs out", 50, 425, arcade.color.WHITE, FONT_SIZE)
-        arcade.draw_text("If your HP hits 0, you lose!", 50, 375, arcade.color.WHITE, FONT_SIZE)
+        arcade.draw_text("If you get hit, you lose!", 50, 375, arcade.color.WHITE, FONT_SIZE)
         arcade.draw_text("Be careful! The enemy can change your controls!", 50, 325, arcade.color.WHITE, FONT_SIZE)
         arcade.draw_text("The red heart can move freely but...", 50, 275, arcade.color.WHITE, FONT_SIZE)
         arcade.draw_text("The purple heart is affected by gravity.", 50, 225, arcade.color.WHITE, FONT_SIZE)
@@ -84,12 +88,14 @@ class GameOver:
                 arcade.draw_text("GAME OVER", 210, 525, arcade.color.WHITE, 50)
 
 
-class Spear(arcade.Sprite):
+class Spears1(arcade.Sprite):
     def update(self):
-        self.center_x -= 3
+        self.center_x -= 4
 
-class Attacks:
-    pass
+
+class Spears2(arcade.Sprite):
+    def update(self):
+        self.center_x += 3
 
 
 class MyGame(arcade.Window):
@@ -110,6 +116,13 @@ class MyGame(arcade.Window):
         # Initialize variables used in the setup function
         self.player_list = None
         self.spear_list1 = None
+        self.spear_list2 = None
+        self.spear_list3 = None
+        self.spear_list4 = None
+        self.spear_list5 = None
+        self.spear_list6 = None
+        self.spear_list7 = None
+        self.spike_list1 = None
         self.player_sprite = None
         self.size = None
         self.center_x = None
@@ -120,7 +133,16 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
+
         self.spear_list1 = arcade.SpriteList()
+        self.spear_list2 = arcade.SpriteList()
+        self.spear_list3 = arcade.SpriteList()
+        self.spear_list4 = arcade.SpriteList()
+        self.spear_list5 = arcade.SpriteList()
+        self.spear_list6 = arcade.SpriteList()
+        self.spear_list7 = arcade.SpriteList()
+
+        self.spike_list1 = arcade.SpriteList()
 
         # Set up the player
         # Heart image from Undertale Wiki
@@ -129,14 +151,34 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 200
         self.player_list.append(self.player_sprite)
 
+        # Set up variables for spear_list1
         self.size = 0.3
-        self.center_x = 600
-        self.center_y = 95
+        self.center_x = 900
+        self.center_y = 75
 
         for i in range(SPEAR_COUNT1):
+            # Set up the spike
+            # Image self made
+            spear = Spears1("images/spear_up.png", self.size)
+            # Center the spike
+            spear.center_x = self.center_x
+            spear.center_y = self.center_y
+            # Add spear to the list of spears
+            self.spear_list1.append(spear)
+            # Change size and position of spear
+            self.size += 0.05
+            self.center_x += 30 + 5*i
+            spear.center_y += 25 + 5*i
+
+        # Set up variables for spear_list2
+        self.size = 0.3
+        self.center_x = -250
+        self.center_y = 330
+
+        for i in range(SPEAR_COUNT2):
             # Set up the spear
             # Image self made
-            spear = Spear("images/spear_up.png", self.size)
+            spear = Spears2("images/spear_down.png", self.size)
             # Center the spear
             spear.center_x = self.center_x
             spear.center_y = self.center_y
@@ -144,9 +186,24 @@ class MyGame(arcade.Window):
             self.spear_list1.append(spear)
             # Change size and position of spear
             self.size += 0.05
-            self.center_x += 20 + 5*i
-            spear.center_y += 25 + 5*i
+            self.center_x += 30 + 5*i
+            spear.center_y -= 25 + 5*i
 
+        self.size = 0.15
+        self.center_x = 335
+        self.center_y = 85
+
+        for i in range(SPIKE_COUNT1):
+            # Set up the spike
+            # Image from pngkit.com
+            spike = arcade.Sprite("images/spikes.png", self.size)
+            # Center the spear
+            spike.center_x = self.center_x
+            spike.center_y = self.center_y
+            # Add spear to the list of spears
+            self.spike_list1.append(spike)
+            # Change size and position of spear
+            self.center_x += 125
 
     def on_draw(self):
         arcade.start_render()
@@ -156,14 +213,20 @@ class MyGame(arcade.Window):
             self.instructions.draw()
         elif PAGE == 3:
             self.player_list.draw()
-            self.spear_list1.draw()
+
+            if TIMER < 29:
+                self.spear_list1.draw()
+                self.spike_list1.draw()
+
+            output = f"timer: {TIMER}"
+            arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
             arcade.draw_rectangle_outline(RECT_CENTER_X, RECT_CENTER_Y, RECT_DISTANCE_FROM_CENTER,
                                           RECT_DISTANCE_FROM_CENTER, arcade.color.WHITE, 1)
         elif PAGE == 4:
             self.game_over.draw()
 
     def update(self, delta_time):
-        global PAGE
+        global PAGE, TIMER, TIMER_INCREMENT
         # Check if the heart is supposed to move
         if (LEFT_PRESSED is True and self.player_sprite.center_x
                 > RECT_CENTER_X - RECT_DISTANCE_FROM_CENTER/2 + PLAYER_MOVEMENT_BORDERS):
@@ -178,13 +241,29 @@ class MyGame(arcade.Window):
                 > RECT_CENTER_Y - RECT_DISTANCE_FROM_CENTER/2 + PLAYER_MOVEMENT_BORDERS):
             self.player_sprite.center_y -= MOVEMENT_SPEED
 
-        self.spear_list1.update()
+        if TIMER < 28:
+            self.spear_list1.update()
+            self.spear_list2.update()
 
         # Generate a list of all sprites that collided with the player.
-        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                              self.spear_list1)
-        if len(coins_hit_list) > 0:
+        player_hit_list_spear1 = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                      self.spear_list1)
+        player_hit_list_spear2 = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                      self.spear_list2)
+
+        player_hit_list_spike1 = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                      self.spike_list1)
+
+        if len(player_hit_list_spear1) > 0 or len(player_hit_list_spear2) > 0 or len(player_hit_list_spike1) > 0:
             PAGE = 4
+        if PAGE == 3:
+            TIMER_INCREMENT += 1
+            if TIMER_INCREMENT >= 60:
+                TIMER -= 1
+                TIMER_INCREMENT = 0
+            if TIMER == 0:
+                PAGE = 5
+
     def on_key_press(self, key, modifiers):
         """ Called whenever a user presses a key """
         global LEFT_PRESSED, RIGHT_PRESSED, UP_PRESSED, DOWN_PRESSED, PAGE
